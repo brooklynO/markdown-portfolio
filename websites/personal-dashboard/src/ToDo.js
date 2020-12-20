66485e828d4cf8c6201c22771dd8ebadd4d70ebc
Brooklyn Brown <43388877+brooklynO@users.noreply.github.com>
2020-12-20 12:08:27 -0800

    Removing cached files

---
+++ websites/personal-dashboard/src/ToDo.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+function ToDo() {
+  return (
+    <h2>ToDo List..</h2>
+  );
+}
+
+export default ToDo;
---
+++ websites/personal-dashboard/src/Weather.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+function Weather() {
+  return (
+    <h2>It is currently...</h2>
+  );
+}
+
+export default Weather;
